4f2154b1b3a6f700c84164ca5731d939c1019f61
ycsome <66495879+ycsome@users.noreply.github.com>
2025-08-30 03:52:32 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftXLSX",
     platforms: [
-        .iOS(.v15_5),
+        .iOS(.v16),
         .macOS(.v12),
     ],
     products: [
